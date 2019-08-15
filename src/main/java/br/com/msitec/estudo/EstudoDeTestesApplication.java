aa7d6e7346b5010e76976ce388a96a72df1a2126
matheus <matheus@DES02.msitec.local>
2019-08-15 17:56:18 -0300

    go

---
+++ src/main/java/br/com/msitec/estudo/EstudoDeTestesApplication.java
@@ -0,0 +1,13 @@
+package br.com.msitec.estudo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudoDeTestesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudoDeTestesApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/msitec/estudo/controller/LocacaoController.java
@@ -0,0 +1,46 @@
+package br.com.msitec.estudo.controller;
+
+import static br.com.msitec.estudo.utils.DataUtils.adicionarDias;
+
+import java.util.Date;
+
+import br.com.msitec.estudo.model.Filme;
+import br.com.msitec.estudo.model.Locacao;
+import br.com.msitec.estudo.model.Usuario;
+import br.com.msitec.estudo.utils.DataUtils;
+
+public class LocacaoController {
+	
+	public Locacao alugarFilme(Usuario usuario, Filme filme) {
+		Locacao locacao = new Locacao();
+		locacao.setFilme(filme);
+		locacao.setUsuario(usuario);
+		locacao.setDataLocacao(new Date());
+		locacao.setValor(filme.getPrecoLocacao());
+
+		//Entrega no dia seguinte
+		Date dataEntrega = new Date();
+		dataEntrega = adicionarDias(dataEntrega, 1);
+		locacao.setDataRetorno(dataEntrega);
+		
+		//Salvando a locacao...	
+		//TODO adicionar método para salvar
+		
+		return locacao;
+	}
+	
+	public static void main(String[] args) {
+		//cenario
+		LocacaoController service = new LocacaoController();
+		Usuario usuario = new Usuario("Usuario 1");
+		Filme filme = new Filme("Filme 1", 2, 5.0);
+		
+		//acao
+		Locacao locacao = service.alugarFilme(usuario, filme);
+		
+		//verificacao
+		System.out.println(locacao.getValor() == 5.0);
+		System.out.println(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
+		System.out.println(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
+	}
+}
